10189cff089a60588dafec7cd9eea01a05e89083
James Dinsdale <hi@molovo.co>
2020-09-18 15:36:41 +0100

    Fix KeyCDN auth

--- src/Provider/KeyCdnProvider.php
+++ src/Provider/KeyCdnProvider.php
@@ -23,9 +23,7 @@ class KeyCdnProvider implements ProviderInterface
 
     public function __construct(string $apiKey, string $zoneId, string $baseUrl)
     {
-        $this->client = new Client([
-            'auth' => [$apiKey.':', ''],
-        ]);
+        $this->client = new Client();
         $this->apiKey  = $apiKey;
         $this->zoneId  = $zoneId;
         $this->baseUrl = $baseUrl;
@@ -36,6 +34,12 @@ class KeyCdnProvider implements ProviderInterface
         return 'KeyCDN';
     }
 
+    protected function getAuth(): array {
+        return [
+            'auth' => [$this->apiKey.':', ''],
+        ]
+    }
+
     public function clearCache(?array $files = []): bool
     {
         if (empty($files)) {
@@ -44,14 +48,12 @@ class KeyCdnProvider implements ProviderInterface
 
         $zoneId   = $this->zoneId;
         $response = $this->client->delete("https://api.keycdn.com/zones/purgeurl/$zoneId.json", [
-            'headers' => [
-                'Content-Type' => 'application/json',
-            ],
-            'body' => json_encode([
+            'auth' => $this->getAuth(),
+            'json' => [
                 'urls' => array_map(function (string $file) use ($baseUrl): string {
                     return $baseUrl.$file;
                 }, $files),
-            ]),
+            ],
         ]);
 
         dump($response);
@@ -60,7 +62,9 @@ class KeyCdnProvider implements ProviderInterface
     public function clearCacheFull(): bool
     {
         $zoneId   = $this->zoneId;
-        $response = $this->client->get("https://api.keycdn.com/zones/purge/$zoneId.json");
+        $response = $this->client->get("https://api.keycdn.com/zones/purge/$zoneId.json", [
+            'auth' => $this->getAuth();
+        ]);
         dump($response);
     }
 }
